783fcc1b17c710bb0dec721230251b85159a6803
Sello Fotoyi <selloclinton@gmail.com>
2021-03-24 12:12:39 +0200

    Confirmation component

---
+++ src/components/Checkout/Confirmation.js
@@ -0,0 +1,47 @@
+import React, {useEffect} from 'react';
+import LoadGif from '../../components/LoadGif';
+import {Link, useHistory} from 'react-router-dom';
+
+const Confirmation = ({
+  order,
+  handleCaptureCheckout,
+  isFinished,
+  shippingData,
+  refreshCart,
+}) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    console.log(shippingData);
+  }, []);
+
+  const goHome = () => {
+    refreshCart();
+    history.push('/');
+  };
+  return (
+    <div className='confirmation'>
+      {order.customer ? (
+        <section>'x</section>
+      ) : isFinished ? (
+        <section>
+          <h2 className='confirmation__title'>
+            Thank you for your purchase, {shippingData.firstName}
+          </h2>
+          <br />
+          <h4 className='confirmation__title'>
+            Please check your email for more details.
+          </h4>
+          <div className='confirmation-btn'>
+            {' '}
+            <button onClick={goHome}>Back to Home</button>
+          </div>
+        </section>
+      ) : (
+        <LoadGif />
+      )}
+    </div>
+  );
+};
+
+export default Confirmation;
